3d8e54cfdeb47412d2bc11125df4e0ae7fb15f47
felipelamela <fe-fl@outlook.com>
2023-07-19 12:07:10 -0300

    Pages arquivos

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,8 @@
 import type { AppProps } from "next/app";
 import GlobalStyle from "../src/GlobalStyle/GlobalStyle";
+import { Roboto } from 'next/font/google'
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,15 @@
+import Footer from "../src/Components/Compositions/Footer/Footer"
+import Header from "../src/Components/Compositions/Header/Header"
 import Main from "../src/Components/Compositions/Main/Main"
 export default function Page() {
-    return (
-      <div>
-        <Main />
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
 
-      </div>
-      //<SectionCards />
+    </>
+    //<SectionCards />
 
-    )
-  }
+  )
+}
